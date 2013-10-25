6f04e9a2cb515c8b072603e477cc2b2bf0ed77c2
Evan Lindsay <evanlindsay@live.com>
2013-10-24 17:50:20 -0700

    Mavenize

---
+++ src/main/java/com/mcprohosting/plugins/mcph_chat_filter/MCPHChatFilter.java
@@ -0,0 +1,23 @@
+package com.mcprohosting.plugins.mcph_chat_filter;
+
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MCPHChatFilter extends JavaPlugin {
+
+	private static Plugin plugin;
+
+	public void onEnable() {
+		// Allow this to be accessed statically
+		plugin = this;
+	}
+
+	public void onDisable() {
+
+	}
+
+	public Plugin getPlugin() {
+		return plugin;
+	}
+
+}
