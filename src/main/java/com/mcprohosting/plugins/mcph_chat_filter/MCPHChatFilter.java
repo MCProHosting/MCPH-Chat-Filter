02f34cfd4a63f85f9f0c062ed1de1a2ed40b142d
Evan Lindsay <evanlindsay@live.com>
2014-03-21 04:04:57 -0700

    Prepare for work

--- src/main/java/com/mcprohosting/plugins/mcph_chat_filter/MCPHChatFilter.java
+++ src/main/java/com/mcprohosting/plugins/mcph_chat_filter/MCPHChatFilter.java
@@ -1,59 +1,22 @@
 package com.mcprohosting.plugins.mcph_chat_filter;
 
-import com.mcprohosting.plugins.mcph_chat_filter.listeners.PlayerListener;
-import com.mcprohosting.plugins.mcph_chat_filter.regex.RegexManager;
 import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.HashMap;
-
 public class MCPHChatFilter extends JavaPlugin {
 
-	private static Plugin plugin;
-	private static HashMap<Player, String> previousMessage;
-	private static RegexManager regexManager;
+    private static Plugin plugin;
 
-	public void onEnable() {
-		// Allow this to be accessed statically
-		plugin = this;
+    public void onEnable() {
+        // Allow this to be accessed statically
+        plugin = this;
 
-		// Initialize previousMessage HashMap
-		previousMessage = new HashMap<Player, String>();
+        // Register listeners
+        Bukkit.getPluginManager().registerEvents(new PlayerListener(), this);
+    }
 
-		// Initialize RegexManager
-		regexManager = new RegexManager();
-
-		// Register Listeners
-		registerListeners();
-	}
-
-	public void onDisable() {
-
-	}
-
-	private void registerListeners() {
-		Bukkit.getPluginManager().registerEvents(new PlayerListener(), this);
-	}
-
-	public static Plugin getPlugin() {
+    public static Plugin getPlugin() {
 		return plugin;
-	}
-
-	public static String getPreviousMessage(Player player) {
-		if (previousMessage.containsKey(player)) {
-			return previousMessage.get(player);
-		}
-		return "";
-	}
-
-	public static void setPreviousMessage(Player player, String message) {
-		previousMessage.put(player, message);
-	}
-
-
-	public static RegexManager getRegexManager() {
-		return regexManager;
 	}
 }
---
+++ src/main/java/com/mcprohosting/plugins/mcph_chat_filter/PlayerListener.java
@@ -0,0 +1,15 @@
+package com.mcprohosting.plugins.mcph_chat_filter;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+public class PlayerListener implements Listener {
+
+    @EventHandler(priority = EventPriority.NORMAL)
+    public void onPlayerChat(AsyncPlayerChatEvent event) {
+
+    }
+
+}
