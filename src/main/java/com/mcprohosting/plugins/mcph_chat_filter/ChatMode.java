b3cf96b77dd5719dc63c706de8a2beb1d64619a5
MCProHosting-Matt <matt@mcprohosting.com>
2014-03-22 13:49:49 -0400

    Fix. Shit.

---
+++ src/main/java/com/mcprohosting/plugins/mcph_chat_filter/ChatMode.java
@@ -0,0 +1,5 @@
+package com.mcprohosting.plugins.mcph_chat_filter;
+
+public enum ChatMode {
+	FREE,SHUTUP,MUTED;
+}
---
+++ src/main/java/com/mcprohosting/plugins/mcph_chat_filter/commands/SetChatMode.java
@@ -0,0 +1,4 @@
+package com.mcprohosting.plugins.mcph_chat_filter.commands;
+
+public class SetChatMode {
+}
